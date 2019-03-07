2a7afaaa94119953b10ba2ea66fd14e526a08978
sadeghsalimi <ssalimi@ce.sharif.edu>
2019-03-07 09:47:29 +0330

    print names of us

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
+        for (int i = 0; i < 3; i++) {
+            switch (i) {
+                case 0:
+                    System.out.println("AmirHosein");
+                    break;
+                case 1:
+                    System.out.println("AliReza");
+                    break;
+                case 2:
+                    System.out.println("Sadegh");
+            }
+        }
     }
 }
